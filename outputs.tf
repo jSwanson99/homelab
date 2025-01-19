f07c2866a7bb1e005f9259ebe5627bfe70a02a20
Jonathan Swanson <jonathan.swanson.dev@gmail.com>
2025-01-18 20:28:49 -0500

    misc

--- outputs.tf
+++ outputs.tf
@@ -6,6 +6,6 @@ output "vault_root_token" {
   value = module.bootstrap.vault_root_token
 }
 
-output "kubernetes_token" {
-  value = module.operational.kubernetes_node_token
-}
+#output "kubernetes_token" {
+#  value = module.operational.kubernetes_node_token
+#}
